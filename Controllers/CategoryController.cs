779f7b3a1d9f0b178af173eb475e05070382ac76
Kobayasidaigon <kurohana2929@outlook.jp>
2023-12-10 23:53:17 +0900

    validの追加

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,45 @@
+using System.Data.Common;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MyMVCapp.Data;
 using MyMVCapp.Models;
 
 namespace MyMVCapp.Controllers;
 
 public class CategoryController : Controller
 {
-    public IActionResult Index(){
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if(obj.Name == obj.DisplayOrder.ToString()){
+            ModelState.AddModelError("name","same match");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Category");
+        }
+
         return View();
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyMVCapp.Models;
@@ -6,7 +7,13 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
-    public string Name { get; set; }
+    [MaxLength(30)]
+    [DisplayName("Display Name")]
+    public string Name { get; set; } = "";
+
+    [Range(1,100)]
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; }
 }
